ea9c75cd1aff5447ea7b0b6a7ff1836d26b2f9f8
[iThesand] <[thesand2009@yandex.ru]>
2021-04-21 20:04:51 +0300

    fixes

--- mainapp/context_processors.py
+++ mainapp/context_processors.py
@@ -4,7 +4,8 @@ from basketapp.models import Basket
 def basket(request):
     basket_list = []
     if request.user.is_authenticated:
-        basket_list = Basket.objects.filter(user=request.user)
+        # basket_list = Basket.objects.filter(user=request.user)
+        basket_list = Basket.get_items(request.user).select_related()
     return {
         'basket': basket_list
     }
