1c15d9a6a9c0005acea521ae19ba434c7c2ff66e
[iThesand] <[thesand2009@yandex.ru]>
2021-04-21 20:24:50 +0300

    fixes

--- mainapp/context_processors.py
+++ mainapp/context_processors.py
@@ -6,6 +6,7 @@ def basket(request):
     if request.user.is_authenticated:
         # basket_list = Basket.objects.filter(user=request.user)
         basket_list = Basket.get_items(request.user).select_related()
+        # basket_list = request.user.basket.select_related()
     return {
         'basket': basket_list
     }
