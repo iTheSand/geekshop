1b165e5aaa5f16c74b408063ede018fb7c2285c8
[iThesand] <[thesand2009@yandex.ru]>
2021-04-23 08:26:28 +0300

    fixes

--- mainapp/context_processors.py
+++ mainapp/context_processors.py
@@ -7,7 +7,7 @@ def basket(request):
         # basket_list = Basket.objects.filter(user=request.user)
 
         # basket_list = Basket.get_items(request.user).select_related()
-        basket_list = Basket.get_items(request.user).select_related()
+        basket_list = Basket.get_items(request.user)
 
         # basket_list = request.user.basket.select_related()
     return {
